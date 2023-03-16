69f1b59bd0e13ced1d6b078b371ef8d33c61b3eb
abodian <ajnbodian@gmail.com>
2023-03-16 21:17:33 +0000

    geolocation fetch request setup

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,10 +1,26 @@
 import './Search.css';
+import { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { CaretRightFill } from 'react-bootstrap-icons';
 import InputGroup from 'react-bootstrap/InputGroup'
+import axios from "axios"
+const API_KEY = process.env.REACT_APP_API_KEY
 
 export function Search() {
+  const [geoLocation, setGeoLocation] = useState(null)
+
+
+  const fetchGeoLocation = () => {
+    return axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}`)
+      .then((response) => setGeoLocation(response.data))
+      .then(console.log(geoLocation))
+  }
+
+  // useEffect(() => {
+  //   fetchGeoLocation();
+  // },[])
+
   return (
     <div className="search-container">
       <img id="desktop-background" src="pattern-bg-desktop.png" alt="Background pattern"/>
